c73b31a8b90c558d6929e12696a0129cb38838d4
Maks Limont <ki212_lme@student.ztu.ua>
2022-07-04 20:14:31 +0300

    merge file starup.cs and program.cs

--- TimeTrackerApp/Program.cs
+++ TimeTrackerApp/Program.cs
@@ -1,26 +1,85 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+﻿using GraphQL.Server;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace TimeTrackerApp
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+
+// In production, the React files will be served from this directory
+builder.Services.AddSpaStaticFiles(configuration =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    configuration.RootPath = "ClientApp/build";
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+
+builder.Services.AddCors(
+    builder => {
+        builder.AddDefaultPolicy(option =>
+        {
+            option.AllowAnyOrigin();
+            option.AllowAnyMethod();
+            option.AllowAnyHeader();
+        });
     }
-}
+);
+
+builder.Services
+    .AddGraphQL(options =>
+    {
+        options.EnableMetrics = true;
+    })
+    .AddSystemTextJson()
+    .AddGraphTypes(/*typeof(ñõåìà)*/);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{ }
+app.UseDeveloperExceptionPage();
+app.UseHsts();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseSpaStaticFiles();
+
+app.UseRouting();
+
+
+app.UseAuthorization();
+app.UseCors(x => x
+         .AllowAnyOrigin()
+         .AllowAnyMethod()
+         .AllowAnyHeader());
+
+app.MapControllers();
+
+/*
+ app.UseGraphQL<Ñõåìà>();
+ */
+app.UseGraphQLAltair();
+
+app.UseSpa(spa =>
+{
+    spa.Options.SourcePath = "ClientApp";
+    if (app.Environment.IsDevelopment())
+    {
+        spa.UseReactDevelopmentServer(npmScript: "start");
+    }
+    
+   
+
+   
+});
+
+app.Run();
