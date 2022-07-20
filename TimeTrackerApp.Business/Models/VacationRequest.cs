7b32d391c5dd8c3582e49221aaff9170d522556c
Oleksandr Igumnov <igumssukr@gmail.com>
2022-07-20 14:07:32 +0300

    VacationRequest model was created

---
+++ TimeTrackerApp.Business/Models/VacationRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace TimeTrackerApp.Business.Models
+{
+	public class VacationRequest
+	{
+		public int Id { get; set; }
+		public DateTime StartingTime { get; set; }
+		public DateTime EndingTime { get; set; }
+		public string? Comment { get; set; } = string.Empty;
+	}
+}
