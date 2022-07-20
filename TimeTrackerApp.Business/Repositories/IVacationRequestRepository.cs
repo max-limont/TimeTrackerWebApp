ba626db6ecf9909fb704cf39e6dabc2fdcc77a9a
Oleksandr Igumnov <igumssukr@gmail.com>
2022-07-20 19:03:38 +0300

    VacationRequest repository methods were created

--- TimeTrackerApp.Business/Repositories/IVacationRequestRepository.cs
+++ TimeTrackerApp.Business/Repositories/IVacationRequestRepository.cs
@@ -5,7 +5,7 @@ namespace TimeTrackerApp.Business.Repositories
 	public interface IVacationRequestRepository
 	{
 		Task<IEnumerable<VacationRequest>> FetchAllAsync();
-		Task<IEnumerable<VacationRequest>> FetchAllUserVacationRequestsAsync();
+		Task<IEnumerable<VacationRequest>> FetchAllUserVacationRequestsAsync(int userId);
 		Task<VacationRequest> GetByIdAsync(int id);
 		Task<VacationRequest> CreateAsync(VacationRequest vacationRequest);
 		Task<VacationRequest> EditAsync(VacationRequest vacationRequest);
