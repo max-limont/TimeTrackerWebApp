5438d943969bac7673a34a8e699c9d5e5aac4576
Oleksandr Igumnov <igumssukr@gmail.com>
2022-07-20 15:19:58 +0300

    Added new field CreatorId to Record model

--- TimeTrackerApp.Business/Models/Record.cs
+++ TimeTrackerApp.Business/Models/Record.cs
@@ -7,6 +7,7 @@
 		public int Id { get; set; }
 		public int WorkingTime { get; set; } = defaultWorkingTime;
 		public string? Comment { get; set; } = string.Empty;
+		public int CreatorId { get; set; }
 		public int? EditorId { get; set; } = null;
 		public DateTime CreatedAt { get; set; } = DateTime.Now;
 	}
