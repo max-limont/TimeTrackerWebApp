68af59c069fc06b773361875270111888d52459e
Oleksandr Igumnov <igumssukr@gmail.com>
2022-07-20 14:02:17 +0300

    Record model was created

---
+++ TimeTrackerApp.Business/Models/Record.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTrackerApp.Business.Models
+{
+	public class Record
+	{
+		private readonly static int defaultWorkingTime = 8 * 60; // 8 hours * 60 minutes
+
+		public int Id { get; set; }
+		public int WorkingTime { get; set; }
+		public string Comment { get; set; } = string.Empty;
+		public int? EditorId { get; set; } = null;
+		public DateTime CreatedAt { get; set; } = DateTime.Now;
+	}
+}
