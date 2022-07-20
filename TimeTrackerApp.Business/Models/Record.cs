d7d8370dc3f3f1a740d8959f8889d676f99c9eec
Oleksandr Igumnov <igumssukr@gmail.com>
2022-07-20 14:03:28 +0300

    Field WorkingTime in model Record was updated

--- TimeTrackerApp.Business/Models/Record.cs
+++ TimeTrackerApp.Business/Models/Record.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TimeTrackerApp.Business.Models
+﻿namespace TimeTrackerApp.Business.Models
 {
 	public class Record
 	{
