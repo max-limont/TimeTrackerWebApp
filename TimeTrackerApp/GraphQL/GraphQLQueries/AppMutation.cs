c7da3e77731aefbc287c88066faf8d3766e9f736
Сергій Денисюк <kn202_dsm@student.ztu.edu.ua>
2022-07-22 17:33:22 +0300

    Maked GraphQL Schema and Types

---
+++ TimeTrackerApp/GraphQL/GraphQLQueries/AppMutation.cs
@@ -0,0 +1,19 @@
+﻿using GraphQL.Types;
+using ToDoList_task.GraphQL.GraphQLTypes;
+using GraphQL;
+
+namespace ToDoList_task.GraphQL.GraphQLQueries
+{
+    public class AppMutation : ObjectGraphType
+    {
+        public AppMutation(
+            IAuthenticationTokenRepository authTokenRep,
+            IRecordRepository recordRep,
+            IUserRepository userRep,
+            IVacationRequestRepository vacationRequestRep
+        )
+        {
+
+        }
+    }
+}
