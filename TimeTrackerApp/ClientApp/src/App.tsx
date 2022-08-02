55da875a8cb5d07596f7403da8673244a7d2663d
Oleksandr Igumnov <igumssukr@gmail.com>
2022-08-02 15:06:47 +0300

    Updated authorization form styles and app routes

--- TimeTrackerApp/ClientApp/src/App.tsx
+++ TimeTrackerApp/ClientApp/src/App.tsx
@@ -3,12 +3,14 @@ import { AuthenticationIndex} from "./components/login/AuthenticationIndex";
 import { Index } from "./components/Layout/Index";
 
 const App = () => {
-   
+
     return (
         <BrowserRouter>
             <Routes>
                 <Route path={"/welcome"} element={<AuthenticationIndex />} />
-                <Route path={"/"} element={<Index />} />
+                <Route path={"/"} element={<Index />} >
+                    <Route path="/calendar" />
+                </Route>
             </Routes>
         </BrowserRouter>
     );
