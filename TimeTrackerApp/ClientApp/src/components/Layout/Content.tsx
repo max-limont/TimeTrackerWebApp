bcc2802769a4a90f79d8396f42dce5e8ab85365a
Maks Limont <91567557+ki212-lme@users.noreply.github.com>
2022-08-02 14:19:39 +0300

    nothing to commit

--- TimeTrackerApp/ClientApp/src/components/Layout/Content.tsx
+++ TimeTrackerApp/ClientApp/src/components/Layout/Content.tsx
@@ -4,12 +4,15 @@ import { refreshTokenKey } from "../../Cookie/Cookie";
 import { parseJwt } from "../../store/parserJWT/parserJWT";
 import { AuthUserResponse } from "../../type/User/AuthUser";
 import Calendar from "../Calendar/Calendar";
+import { Home } from "./Home";
 
 export const Content: FC = () => {
 
     return (
         <div className={"content-container flex-container w-100"}>
             <Routes>
+                <Route index element={<Home />} />
+                <Route path={"/calendar"} element={<Calendar />} />
             </Routes>
         </div>
     );
--- TimeTrackerApp/ClientApp/src/components/Layout/Header.tsx
+++ TimeTrackerApp/ClientApp/src/components/Layout/Header.tsx
@@ -1,13 +1,8 @@
-
 import { FC, useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { fetchUserByIdAction } from "../../store/actions/user/userActions";
 
 export const Header: FC = () => {
-    const dispatch = useAppDispatch();
-    const [user, setUser] = useState(useAppSelector(s => s.rootReducer.auth.user));
-
-
     return (
         <header className={"personal-account-header header flex-container w-100"}>
             <div className={"breadcrumbs flex-container"}>
@@ -23,5 +18,3 @@ export const Header: FC = () => {
         </header>
     );
 }
-
-
