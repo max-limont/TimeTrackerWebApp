e415f5c080e4cc5ef16726adb4fd4f28c6a60db9
Oleksandr Igumnov <igumssukr@gmail.com>
2022-08-09 16:09:15 +0300

    Added user account link

--- TimeTrackerApp/ClientApp/src/components/Layout/Header.tsx
+++ TimeTrackerApp/ClientApp/src/components/Layout/Header.tsx
@@ -1,5 +1,6 @@
 import {FC} from "react";
 import {useAuth} from "../../hooks/useAuth";
+import {Link} from "react-router-dom";
 
 export const Header: FC = () => {
 
@@ -14,8 +15,10 @@ export const Header: FC = () => {
                 </nav>
             </div>
             <div className={"user-info flex-container"}>
-                <img src={`${process.env.PUBLIC_URL}/images/ava.jpg`} alt={"user-profile-image"} />
-                <p>{auth.state?.user?.firstName} {auth.state?.user?.lastName}</p>
+                <Link to={'/user-page'} replace className={"flex-container align-items-center"}>
+                    <img src={`${process.env.PUBLIC_URL}/images/ava.jpg`} alt={"user-profile-image"} />
+                    <p>{auth.state?.user?.firstName} {auth.state?.user?.lastName}</p>
+                </Link>
             </div>
         </header>
     );
