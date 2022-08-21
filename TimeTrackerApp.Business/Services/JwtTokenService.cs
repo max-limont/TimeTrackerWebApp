84eea914a3eb74f15f1cb90dab752cb9d400b5e7
Oleksandr Igumnov <igumssukr@gmail.com>
2022-08-21 22:21:20 +0300

    Added new table in database 'BackgroundTask'. Created 'BackgroundTaskService'

--- TimeTrackerApp.Business/Services/JwtTokenService.cs
+++ TimeTrackerApp.Business/Services/JwtTokenService.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
-using System.Security.Cryptography;
 using System.Security.Claims;
 using System.Text;
 using TimeTrackerApp.Business.Models;
---
+++ TimeTrackerApp/Services/BackgroundTaskService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Hosting;
+using Quartz;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TimeTrackerApp.Services
+{
+	public class BackgroundTaskService : IHostedService
+	{
+		private readonly ISchedulerFactory schedulerFactory;
+
+		public BackgroundTaskService(ISchedulerFactory schedulerFactory)
+		{
+			this.schedulerFactory = schedulerFactory;
+		}
+
+		public Task StartAsync(CancellationToken cancellationToken)
+		{
+			throw new Exception();
+		}
+
+		public Task StopAsync(CancellationToken cancellationToken)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
