a87f94110cc775335220dac3294a11287128e255
Oleksandr Igumnov <igumssukr@gmail.com>
2022-08-21 22:47:06 +0300

    Created 'BackgroundTaskRepository'

---
+++ TimeTrackerApp.Business/Repositories/IBackgroundTaskRepository.cs
@@ -0,0 +1,15 @@
+﻿using TimeTrackerApp.Business.Models;
+
+namespace TimeTrackerApp.Business.Repositories
+{
+	public interface IBackgroundTaskRepository
+	{
+		Task<IEnumerable<BackgroundTask>> FetchAllAsync();
+		Task<IEnumerable<BackgroundTask>> FetchByTypeAsync(string type);
+		Task<IEnumerable<BackgroundTask>> FetchByDateTimeAsync(DateTime dateTime);
+		Task<BackgroundTask> GetLatestAsync();
+		Task<BackgroundTask> GetByIdAsync(int id);
+		Task<BackgroundTask> CreateAsync(BackgroundTask task);
+		Task<BackgroundTask> RemoveAsync(int id);
+	}
+}
