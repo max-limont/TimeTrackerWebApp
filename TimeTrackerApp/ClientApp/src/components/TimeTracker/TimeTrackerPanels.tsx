0dc9bb2a42f955fefb5ba0da8e39b5be75113a95
Oleksandr Igumnov <igumssukr@gmail.com>
2022-08-23 15:51:37 +0300

    Fixed automatical accrual of working time

--- TimeTrackerApp/ClientApp/src/components/TimeTracker/TimeTrackerPanels.tsx
+++ TimeTrackerApp/ClientApp/src/components/TimeTracker/TimeTrackerPanels.tsx
@@ -4,6 +4,40 @@ import {useAuth} from "../../hooks/useAuth";
 import {useAppSelector} from "../../hooks/useAppSelector";
 import {useDispatch} from "react-redux";
 import {updateCurrentWeekWorkingTime} from "../../store/timeTracker/timeTracker.slice";
+import {Statistic, StatisticPropsType} from "./Statistic";
+
+const statisticData: StatisticPropsType = {
+    metadata: {
+        maxValue: 8 * 60 * 60 * 1000,
+        minValue: -3 * 60 * 60 * 1000,
+        gap: 50,
+        itemWidth: 50,
+        width: 400,
+        height: 200
+    },
+    data: [
+        {
+            title: "Mon",
+            value: 60 * 60 * 1000
+        },
+        {
+            title: "Tue",
+            value: -3 * 60 * 60 * 1000
+        },
+        {
+            title: "Wed",
+            value: 4 * 60 * 60 * 1000
+        },
+        {
+            title: "Thu",
+            value: 6 * 60 * 60 * 1000
+        },
+        {
+            title: "Fri",
+            value: 10 * 60 * 60 * 1000
+        }
+    ]
+}
 
 export const TimeTrackerPanels: FC = () => {
 
@@ -30,7 +64,7 @@ export const TimeTrackerPanels: FC = () => {
     return !auth.state?.user?.isFullTimeEmployee ? (
         <div className={`time-tracker-statistic`}>
             <Timer />
-            <div className={"statistic-panel"}>
+            <div className={"statistic-panel flex-container flex-column"}>
                 <h3>This week activity:</h3>
                 <div className={"statistic-panel-list"}>
                     <div>
@@ -49,4 +83,5 @@ export const TimeTrackerPanels: FC = () => {
             </div>
         </div>
     ) : <></>
+    // <Statistic data={statisticData.data} metadata={statisticData.metadata} />
 }
