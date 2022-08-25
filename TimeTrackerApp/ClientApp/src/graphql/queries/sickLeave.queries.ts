0c914154084829c103fe5aa95040d4ce7f65f973
Oleksandr Igumnov <igumssukr@gmail.com>
2022-08-25 15:54:30 +0300

    Added sick leaves graphql queries and mutations on frontend

---
+++ TimeTrackerApp/ClientApp/src/graphql/queries/sickLeave.queries.ts
@@ -0,0 +1,97 @@
+export const fetchAllSickLeavesQuery = `
+    query FetchAllSickLeaves {
+        fetchAllSickLeaves {
+            id
+            startDate
+            endDate
+            employeeId
+            approverId
+            status
+            creationDateTime
+        }
+    }
+`
+
+export const fetchAllSickLeavesByEmployeeIdQuery = `
+    query FetchAllSickLeavesByEmployeeId($employeeId: ID!) {
+        fetchAllSickLeavesByEmployeeId(employeeId: $employeeId) {
+            id
+            startDate
+            endDate
+            employeeId
+            approverId
+            status
+            creationDateTime
+        }
+    }
+`
+
+export const fetchAllSickLeavesForManagerByManagerIdQuery = `
+    query FetchAllSickLeavesForManagerByManagerId($managerId: ID!) {
+        fetchAllSickLeavesForManagerByManagerId(managerId: $managerId) {
+            id
+            startDate
+            endDate
+            employeeId
+            approverId
+            status
+            creationDateTime
+        }
+    }
+`
+
+export const getSickLeaveByIdQuery = `
+    query GetSickLeaveById($id: ID!) {
+        getSickLeaveById(id: $id) {
+            id
+            startDate
+            endDate
+            employeeId
+            approverId
+            status
+            creationDateTime
+        }
+    }
+`
+
+export const createSickLeaveMutation = `
+    mutation CreateSickLeave($sickLeave: SickLeaveInputType!) {
+        createSickLeave(sickLeave: $sickLeave) {
+            id
+            startDate
+            endDate
+            employeeId
+            approverId
+            status
+            creationDateTime
+        }
+    }
+`
+
+export const updateSickLeaveMutation = `
+    mutation EditSickLeave($sickLeave: SickLeaveInputType!) {
+        editSickLeave(sickLeave: $sickLeave) {
+            id
+            startDate
+            endDate
+            employeeId
+            approverId
+            status
+            creationDateTime
+        }
+    }
+`
+
+export const removeSickLeaveMutation = `
+    mutation RemoveSickLeave($id: ID!) {
+        removeSickLeave(id: $id) {
+            id
+            startDate
+            endDate
+            employeeId
+            approverId
+            status
+            creationDateTime
+        }
+    }
+`
