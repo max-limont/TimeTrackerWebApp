4f065a91b3a1c53db161cd00f53b944049383345
Oleksandr Igumnov <igumssukr@gmail.com>
2022-08-25 14:19:37 +0300

    Added SickLeave model on backend

---
+++ TimeTrackerApp.Business/Models/SickLeave.cs
@@ -0,0 +1,15 @@
+﻿using TimeTrackerApp.Business.Enums;
+
+namespace TimeTrackerApp.Business.Models
+{
+	public class SickLeave
+	{
+		public int Id { get; set; }
+		public DateTime StartDate { get; set; }
+		public DateTime EndDate { get; set; }
+		public int EmployeeId { get; set; }
+		public int? ApproverId { get; set; }
+		public SickLeaveStatuses Status { get; set; }
+		public DateTime CreatedAt { get; set; }
+	}
+}
