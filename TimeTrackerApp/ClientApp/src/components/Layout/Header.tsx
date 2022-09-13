b246778eac1da6e33db2324673d85890be554d43
ra1den02 <ipz201_vog@student.ztu.edu.ua>
2022-09-13 15:33:19 +0300

    Updated UserPage

--- TimeTrackerApp/ClientApp/src/components/Layout/Header.tsx
+++ TimeTrackerApp/ClientApp/src/components/Layout/Header.tsx
@@ -1,11 +1,12 @@
 import {FC} from "react";
 import {useAuth} from "../../hooks/useAuth";
 import {Link} from "react-router-dom";
+import {useAppSelector} from "../../hooks/useAppSelector";
 
 export const Header: FC = () => {
 
+    const user = useAppSelector((x) => x.rootReducer.user.user)
     const auth = useAuth()
-
     return (
         <header className={"personal-account-header header flex-container"}>
             <div className={"breadcrumbs flex-container"}>
@@ -18,6 +19,7 @@ export const Header: FC = () => {
                 <Link to={'/user-page'} replace className={"flex-container align-items-center"}>
                     <img src={`${process.env.PUBLIC_URL}/images/ava.jpg`} alt={"user-profile-image"}/>
                     <p>{auth.state?.user?.firstName} {auth.state?.user?.lastName}</p>
+
                 </Link>
             </div>
         </header>
